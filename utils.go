ddfa09e7ada15023c5735cbfa99baf085b8404ce
Shiny Nematoda <snematoda@noreply.codeberg.org>
2022-12-13 08:38:56 +0000

    [BREAKING] HYP_PROXY only contains the hostname

--- utils.go
+++ utils.go
@@ -31,7 +31,7 @@ func ParseUrl(raw string) (string, error) {
 
 	prehost := u.Host
 
-	u.Host = os.Getenv("HYP_PROXY")
+	u.Host = "https://" + os.Getenv("HYP_PROXY")
 	q := u.Query()
 	q.Set("host", prehost)
 	u.RawQuery = q.Encode()
